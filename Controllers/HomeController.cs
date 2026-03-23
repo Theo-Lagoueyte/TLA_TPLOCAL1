443c28581c0e4810a0d6c2ec675e0477b75f049c
Theo-Lagoueyte <theo.lagoueyte1@gmail.com>
2026-03-23 12:06:42 +0100

    maj validation du formulaire

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
 using TPLOCAL1.Models;
+using System.Text.RegularExpressions;
 
 //Subject is find at the root of the project and the logo in the wwwroot/ressources folders of the solution
 //--------------------------------------------------------------------------------------
@@ -27,6 +28,7 @@ namespace TPLOCAL1.Controllers
                         return View(id);
                     case "Form":
                         //TODO : call the Form view with data model empty
+                        FormInfos formInfos = new();
                         return View(id);
                     default:
                         //retourn to the Index view (see routing in Program.cs)
@@ -38,13 +40,42 @@ namespace TPLOCAL1.Controllers
 
         //methode to send datas from form to validation page
         [HttpPost]
-        public ActionResult ValidationFormulaire(/*model*/)
+        public ActionResult ValidationFormulaire(/*model*/FormInfos formInfos)
         {
             //TODO : test if model's fields are set
             //if not, display an error message and stay on the form page
             //else, call ValidationForm with the datas set by the user
-            return null;
+            formInfos.Nom = Request.Form["nom"];
+            formInfos.Prenom = Request.Form["prenom"];
+            formInfos.Sexe = Request.Form["sexe"];
+            formInfos.Adresse = Request.Form["adresse"];
+            formInfos.CodePostal = Request.Form["codePostal"];
+            System.Text.RegularExpressions.Regex codePostalRegex = new System.Text.RegularExpressions.Regex(@"^(\w{5})$");
+            formInfos.Ville = Request.Form["ville"];            
+            formInfos.AdresseMail = Request.Form["mail"];
+            System.Text.RegularExpressions.Regex mailRegex = new System.Text.RegularExpressions.Regex(@"^([\w]+)@([\w]+)\.([\w]+)$");            
+            formInfos.DateDebut = Request.Form["dateDebut"];
+            formInfos.FormationSuivie = Request.Form["formation"];
+            formInfos.AvisCobol = Request.Form["avisCobol"];
+            formInfos.AvisCSharp = Request.Form["avisCSharp"];
 
+            //validation de l'adresse mail
+            if (!mailRegex.IsMatch(formInfos.AdresseMail))
+            {
+                System.Console.WriteLine("Adresse mail invalide");
+                return View("Form");
+            }
+            //validation du code postal
+            else if (!codePostalRegex.IsMatch(formInfos.CodePostal))
+            {
+                System.Console.WriteLine("Code postal invalide");
+                return View("Form");
+            }
+            //si tout est bon on passe à la page de validation
+            else
+            {
+                return View("Validation", FormInfos);
+            }
         }
     }
 }
